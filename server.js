fd886f807885b3f0ff9e179fda35f3c70073783e
Dipak <sinhadipak33@gmail.com>
2022-01-18 19:22:43 +0530

    Complete assignment code

---
+++ server.js
@@ -0,0 +1,121 @@
+// Importing the module
+const axios = require('axios')
+const csv = require('csv-writer')
+const readline = require("readline-sync");
+
+var endDate = new Date();
+var dateColumns = [];
+
+
+
+/**
+ * Function to calculate total days diffence in two dates
+ * @param {Date} date1 Today's Date
+ * @param {Date} date2 End Date
+ * @returns Total days
+ */
+function getDifferenceInDays(date1, date2) {
+    const diffInMs = Math.abs(date2 - date1);
+    return diffInMs / (1000 * 60 * 60 * 24);
+}
+
+/**
+ * Function to make Date column for csv file
+ * @param {Date} startDate Start Date
+ * @param {Date} endDate End Date
+ */
+
+function dateColumn(startDate, endDate) {
+    while (startDate < endDate) {
+        let date1 = new Date(startDate);
+        startDate = date1.setDate(date1.getDate() + 1);
+        dateColumns.push(date1.toLocaleDateString('en-US', { year: '2-digit', month: '2-digit', day: '2-digit' }));
+    }
+}
+// Input here address and page size
+let pageSize = Number(readline.question("Enter Page Size: "));
+let address = readline.question("Enter Address: ");
+
+axios
+    .post('https://www.vrbo.com/serp/g', {
+        "operationName": "SearchRequestQuery",
+        "variables": {
+            "filterCounts": true,
+            "request": {
+                "paging": {
+                    "page": 1,
+                    "pageSize": pageSize
+                },
+                "filterVersion": "1",
+                "coreFilters": {
+                    "adults": 1,
+                    "maxBathrooms": null,
+                    "maxBedrooms": null,
+                    "maxNightlyPrice": null,
+                    "maxTotalPrice": null,
+                    "minBathrooms": 0,
+                    "minBedrooms": 0,
+                    "minNightlyPrice": 0,
+                    "minTotalPrice": null,
+                    "pets": 0
+                },
+                "filters": [],
+                "q": address
+            },
+            "optimizedBreadcrumb": false,
+            "vrbo_web_global_messaging_banner": true
+        },
+        "extensions": {
+            "isPageLoadSearch": true
+        },
+        "query": "query SearchRequestQuery($request: SearchResultRequest!, $filterCounts: Boolean!, $optimizedBreadcrumb: Boolean!, $vrbo_web_global_messaging_banner: Boolean!) {  results: search(request: $request) {    ...querySelectionSet    ...DestinationBreadcrumbsSearchResult    ...DestinationMessageSearchResult    ...FilterCountsSearchRequestResult    ...HitCollectionSearchResult    ...ADLSearchResult    ...MapSearchResult    ...ExpandedGroupsSearchResult    ...PagerSearchResult    ...SearchTermCarouselSearchResult    ...InternalToolsSearchResult    ...SEOMetaDataParamsSearchResult    ...GlobalInlineMessageSearchResult    ...GlobalBannerContainerSearchResult @include(if: $vrbo_web_global_messaging_banner)    __typename  }}fragment querySelectionSet on SearchResult {  id  typeaheadSuggestion {    uuid    term    name    __typename  }  geography {    lbsId    gaiaId    location {      latitude      longitude      __typename    }    isGeocoded    shouldShowMapCentralPin    __typename  }  propertyRedirectUrl  __typename}fragment DestinationBreadcrumbsSearchResult on SearchResult {  destination(optimizedBreadcrumb: $optimizedBreadcrumb) {    breadcrumbs {      name      url      __typename    }    __typename  }  __typename}fragment HitCollectionSearchResult on SearchResult {  page  pageSize  pageCount  queryUUID  percentBooked {    currentPercentBooked    __typename  }  listings {    ...HitListing    __typename  }  resultCount  pinnedListing {    headline    listing {      ...HitListing      __typename    }    __typename  }  __typename}fragment HitListing on Listing {  virtualTourBadge {    name    id    helpText    __typename  }  amenitiesBadges {    name    id    helpText    __typename  }  images {    altText    c6_uri    c9_uri    mab {      banditId      payloadId      campaignId      cached      arm {        level        imageUrl        categoryName        __typename      }      __typename    }    __typename  }  ...HitInfoListing  __typename}fragment HitInfoListing on Listing {  listingId  ...HitInfoDesktopListing  ...HitInfoMobileListing  ...PriceListing  __typename}fragment HitInfoDesktopListing on Listing {  detailPageUrl unitApiUrl  instantBookable  minStayRange {    minStayHigh    minStayLow    __typename  }  listingId  listingNumber  rankedBadges(rankingStrategy: SERP) {    id    helpText    name    __typename  }  propertyId  propertyMetadata {    headline    __typename  }  superlativesBadges: rankedBadges(rankingStrategy: SERP_SUPERLATIVES) {    id    helpText    name    __typename  }  unitMetadata {    unitName    __typename  }  webRatingBadges: rankedBadges(rankingStrategy: SRP_WEB_RATING) {    id    helpText    name    __typename  }  ...DetailsListing  ...GeoDistanceListing  ...RateSummary ...PriceListing  ...RatingListing  __typename}fragment DetailsListing on Listing {  bathrooms {    full    half    toiletOnly    __typename  }  bedrooms  propertyType  sleeps  petsAllowed  spaces {    spacesSummary {      area {        areaValue        __typename      }      bedCountDisplay      __typename    }    __typename  }  __typename}fragment GeoDistanceListing on Listing {  geoDistance {    text    relationType    __typename  }  __typename}  fragment RateSummary on Listing { rateSummary { beginDate  endDate rentNights } } fragment PriceListing on Listing {  priceSummary: priceSummary {  priceAccurate    ...PriceSummaryTravelerPriceSummary    __typename  }  priceSummarySecondary: priceSummary(summary: \"displayPriceSecondary\") {    ...PriceSummaryTravelerPriceSummary    __typename  }  priceLabel: priceSummary(summary: \"priceLabel\") {    priceTypeId    pricePeriodDescription    __typename  }  prices {    ...VrboTravelerPriceSummary    __typename  }  __typename}fragment PriceSummaryTravelerPriceSummary on TravelerPriceSummary {  priceTypeId  edapEventJson  formattedAmount  roundedFormattedAmount  pricePeriodDescription  __typename}fragment VrboTravelerPriceSummary on PriceSummary {  perNight {    amount    formattedAmount    roundedFormattedAmount    pricePeriodDescription    __typename  }  total {    amount    formattedAmount    roundedFormattedAmount    pricePeriodDescription    __typename  }  label  mainPrice  __typename}fragment RatingListing on Listing {  averageRating  reviewCount  __typename}fragment HitInfoMobileListing on Listing {  detailPageUrl  instantBookable  minStayRange {    minStayHigh    minStayLow    __typename  }  listingId  listingNumber  rankedBadges(rankingStrategy: SERP) {    id    helpText    name    __typename  }  propertyId  propertyMetadata {    headline    __typename  }  superlativesBadges: rankedBadges(rankingStrategy: SERP_SUPERLATIVES) {    id    helpText    name    __typename  }  unitMetadata {    unitName    __typename  }  webRatingBadges: rankedBadges(rankingStrategy: SRP_WEB_RATING) {    id    helpText    name    __typename  }  ...DetailsListing  ...GeoDistanceListing ...RateSummary ...PriceListing  ...RatingListing  __typename}fragment ExpandedGroupsSearchResult on SearchResult {  expandedGroups {    ...ExpandedGroupExpandedGroup    __typename  }  __typename}fragment ExpandedGroupExpandedGroup on ExpandedGroup {  listings {    ...HitListing    ...MapHitListing    __typename  }  mapViewport {    neLat    neLong    swLat    swLong    __typename  }  __typename}fragment MapHitListing on Listing {  ...HitListing  geoCode {    latitude    longitude    __typename  }  __typename}fragment FilterCountsSearchRequestResult on SearchResult {  id  resultCount  filterGroups {    groupInfo {      name      id      __typename    }    filters {      count @include(if: $filterCounts)      checked      filter {        id        name        refineByQueryArgument        description        __typename      }      __typename    }    __typename  }  __typename}fragment MapSearchResult on SearchResult {  mapViewport {    neLat    neLong    swLat    swLong    __typename  }  page  pageSize  listings {    ...MapHitListing    __typename  }  pinnedListing {    listing {      ...MapHitListing      __typename    }    __typename  }  __typename}fragment PagerSearchResult on SearchResult {  fromRecord  toRecord  pageSize  pageCount  page  resultCount  __typename}fragment DestinationMessageSearchResult on SearchResult {  destinationMessage(assetVersion: 4) {    iconTitleText {      title      message      icon      messageValueType      link {        linkText        linkHref        __typename      }      __typename    }    ...DestinationMessageDestinationMessage    __typename  }  __typename}fragment DestinationMessageDestinationMessage on DestinationMessage {  iconText {    message    icon    messageValueType    __typename  }  __typename}fragment ADLSearchResult on SearchResult {  parsedParams {    q    coreFilters {      adults      children      pets      minBedrooms      maxBedrooms      minBathrooms      maxBathrooms      minNightlyPrice      maxNightlyPrice      minSleeps      __typename    }    dates {      arrivalDate      departureDate      __typename    }    sort    __typename  }  page  pageSize  pageCount  resultCount  fromRecord  toRecord  pinnedListing {    listing {      listingId      __typename    }    __typename  }  listings {    listingId    __typename  }  filterGroups {    filters {      checked      filter {        groupId        id        __typename      }      __typename    }    __typename  }  geography {    lbsId    name    description    location {      latitude      longitude      __typename    }    primaryGeoType    breadcrumbs {      name      countryCode      location {        latitude        longitude        __typename      }      primaryGeoType      __typename    }    __typename  }  __typename}fragment SearchTermCarouselSearchResult on SearchResult {  discoveryXploreFeeds {    results {      id      title      items {        ... on SearchDiscoveryFeedItem {          type          imageHref          place {            uuid            name {              full              simple              __typename            }            __typename          }          __typename        }        __typename      }      __typename    }    __typename  }  typeaheadSuggestion {    name    __typename  }  __typename}fragment InternalToolsSearchResult on SearchResult {  internalTools {    searchServiceUrl    __typename  }  __typename}fragment SEOMetaDataParamsSearchResult on SearchResult {  page  resultCount  pageSize  geography {    name    lbsId    breadcrumbs {      name      __typename    }    __typename  }  __typename}fragment GlobalInlineMessageSearchResult on SearchResult {  globalMessages {    ...GlobalInlineAlertGlobalMessages    __typename  }  __typename}fragment GlobalInlineAlertGlobalMessages on GlobalMessages {  alert {    action {      link {        href        text {          value          __typename        }        __typename      }      __typename    }    body {      text {        value        __typename      }      link {        href        text {          value          __typename        }        __typename      }      __typename    }    id    severity    title {      value      __typename    }    __typename  }  __typename}fragment GlobalBannerContainerSearchResult on SearchResult {  globalMessages {    ...GlobalBannerGlobalMessages    __typename  }  __typename}fragment GlobalBannerGlobalMessages on GlobalMessages {  banner {    body {      text {        value        __typename      }      link {        href        text {          value          __typename        }        __typename      }      __typename    }    id    severity    title {      value      __typename    }    __typename  }  __typename}"
+    })
+    .then(res => {
+        var records = []; //records for csv file
+
+        //loop in result data
+        for (const element of res.data.data.results.listings) {
+            let row = [];
+            if (element.rateSummary != null && element.rateSummary.rentNights != null) {
+
+                const date1 = new Date('2022-01-18');//Todays date
+                const date2 = new Date(element.rateSummary.endDate);//End date
+                const index = getDifferenceInDays(date1, date2);//Get no of days
+                //Slice array of rentNight from TodaysDate to endDate
+                let arr = element.rateSummary.rentNights.slice(index - 1, element.rateSummary.rentNights.length);
+
+                //Push the elements to 1D array
+                row.push(element.listingId);
+                row.push(element.propertyMetadata.headline);
+                row = row.concat(arr);
+
+                //Push the row to 2D array
+                records.push(row);
+
+                //To get max endDate 
+                if (date2 > endDate) {
+                    endDate = date2;
+                }
+            }
+        }
+
+
+        const currentDate = new Date();//get cuurent date
+
+        dateColumns.push('Unit id', 'Name');//csv file datecolumns name array 
+        dateColumn(currentDate, endDate);// get all date from todaysDate to endDate in dateColumns name array
+
+        //Create csv file 
+        const createCsvWriter = csv.createArrayCsvWriter;
+        const csvWriter = createCsvWriter({
+            header: dateColumns,
+            path: `file_${parseInt(Math.random() * 1000)}.csv`
+        });
+        csvWriter.writeRecords(records)       // returns a promise
+            .then(() => {
+                console.log('Successfully created csv file');
+            });
+    })
+    .catch(error => {
+        console.error(error)
+    })
